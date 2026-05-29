de100630e5643e7399d114bba8b60f2542b5e82c
Wizard732 <bvdovenko567@gmail.com>
2026-05-29 10:39:50 +0300

    I created a GET request and connected to SQL

--- app/config.py
+++
@@ -1,4 +0,0 @@
-HOST = "127.0.0.1"
-DATABASE = "learn_sql"  # 2 db Electronics_bd
-USERNAME = "wizard" # data sql
-PASSWORD = "5732"
--- handlers/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-class Data(BaseModel):
-    id: int
-    name: str = Field(max_length=20)
-    price: int = Field(gt=5)
-    stock_quantity: int = Field(gt=0)
-
